c4572ab484bc2ee9050c617e1a3aa964324caa43
darinakruk <kruk.darina@gmail.com>
2017-02-01 12:26:12 +0200

    add object

--- files/personal_account/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o,p,q,r,s,t],u,_(v,w,x,y,z,A,B,_(),C,_(D,E,F,G,H,_(I,J,K,L),M,null,N,G,O,G,P,Q,R,null,S,T,U,V,W,X,Y,T),Z,_(ba,_(),bb,_(),bc,_(),bd,_(),be,_()),bf,_(),bg,_(bh,[])),bi,_(),bj,_());}; 
-var b="url",c="personal_account.html",d="generationDate",e=new Date(1482854530144.16),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="AxStWidget",l="AxStYLimit",m="AxStRowHeight",n="AxStYPosition",o="AxStHighest",p="AxStPickedValue",q="AxStXPosition",r="AxStXLimit",s="AxStSwipeMomentum",t="AxStYRotateDirection",u="page",v="packageId",w="be2d1269518b4c0aba99c7e25cf5ed5b",x="type",y="Axure:Page",z="name",A="Personal Account",B="notes",C="style",D="baseStyle",E="627587b6038d43cca051c114ac41ad32",F="pageAlignment",G="near",H="fill",I="fillType",J="solid",K="color",L=0xFFFFFFFF,M="image",N="imageHorizontalAlignment",O="imageVerticalAlignment",P="imageRepeat",Q="auto",R="favicon",S="sketchFactor",T="0",U="colorStyle",V="appliedColor",W="fontName",X="Applied Font",Y="borderWidth",Z="adaptiveStyles",ba="a",bb="b",bc="c",bd="d",be="e",bf="interactionMap",bg="diagram",bh="objects",bi="masters",bj="objectPaths";
-return _creator();
-})());
